2768c47f01e827bf53af4b56232a5d4b10812495
mmusaev <9410421+mmusaev@users.noreply.github.com>
2026-02-01 15:34:59 -0600

    chore: Bump version to 2.0.0 (major version increment)
    BREAKING CHANGE: Incremented major version from 1.1.0 to 2.0.0
    
    - Updated Directory.Build.props: VersionMajor=2, VersionMinor=0, VersionPatch=0
    - Updated SharedAssemblyInfo.cs: AssemblyVersion, FileVersion, and InformationalVersion to 2.0.0
    - All projects now build with version 2.0.0.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 // Version information - NOTE: Also defined in Directory.Build.props
 // For local development: Manually sync these with Directory.Build.props when bumping version
 // For CI/CD builds: azure-pipelines.yml automatically updates these values
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
